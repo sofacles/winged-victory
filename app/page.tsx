f31ae958f89801f19b4c5986eb6df181760fa52f
sofacles <sofacles@yahoo.com>
2026-04-06 22:11:59 -0700

    fix background positioning for my laptop screen

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className=" bg-white">
-      <div className="nike"></div>
+    <div className="bg-white">
+      <div className="nike">
+        <h1>Winged Victory Painting</h1>
+        <p>
+          I specialize in interior painting and finishing.  I'm cheap because I'm actually a software engineer who used to work in a furniture finishing shop and painted apartments on the side.
+          I received good training at the finishing shop, and can see mistakes that other people miss.  So, I won't be cheap for long... or at least that's my plan.  I guarantee my work for 2 years.
+        </p>
+      </div>
 
     </div>
   );
